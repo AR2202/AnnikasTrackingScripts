9d2e11a508e46948c784f48189655da19eb91bc0
Annika Rings <annika.rings@dpag.ox.ac.uk>
2018-09-14 17:12:33 +0100

    added criterion of 0.5s consecutive wing extention to handle_flytracker_outputs.m

--- handle_flytracker_outputs.m
+++ handle_flytracker_outputs.m
@@ -18,4 +18,16 @@ load(filename);
 indices=transpose(1:size(feat.data,1));
 ind_data=arrayfun(@(x) horzcat(transpose(feat.data(x,:,1)),transpose(feat.data(x,:,2)),transpose(feat.data(x,:,3)),transpose(feat.data(x,:,4)),transpose(feat.data(x,:,5)),transpose(feat.data(x,:,6)),transpose(feat.data(x,:,7)),transpose(feat.data(x,:,8)),transpose(feat.data(x,:,9)),transpose(feat.data(x,:,10)),transpose(feat.data(x,:,11)),transpose(feat.data(x,:,12)),transpose(feat.data(x,:,13))),indices,'UniformOutput',false);
 wing_ext_frames_all=cellfun(@(indiv) indiv(indiv(:,4)>pi/6,:),ind_data,'UniformOutput',false);
-wing_ext_frames_indexed=cellfun(@(cell1,cell2) {cell1,cell2}, wing_ext_frames_all,num2cell(indices),'UniformOutput',false);
+aboveThreshold=cellfun(@(indiv) (indiv(:,4)>pi/6),ind_data,'UniformOutput',false);
+aboveThreshold=cellfun(@(above) [false;above;false],aboveThreshold,'uni',false);
+edges = cellfun(@(above) diff(above),aboveThreshold,'UniformOutput',false);
+rising = cellfun(@(edge) find(edge==1),edges,'UniformOutput',false);    %rising/falling edges
+falling = cellfun(@(edge) find(edge==-1),edges,'UniformOutput',false);    %rising/falling edges
+spanWidth = cellfun(@(rise,fall) fall-rise,rising,falling,'UniformOutput',false);  %width of span of 1's (above threshold)
+wideEnough = cellfun(@(span) span >= 13,spanWidth,'UniformOutput',false);    
+startPos = cellfun(@(rise,wideenough) transpose(rise(wideenough)),rising,wideEnough,'UniformOutput',false);    %start of each span
+endPos = cellfun(@(fall,wideenough) transpose(fall(wideenough)-1),falling,wideEnough,'UniformOutput',false);  %end of each span
+allInSpan = cellfun(@(starts,ends) arrayfun(@(x,y) (x:1:y), starts, ends, 'uni', false),startPos,endPos,'uni',false);  
+catspans=cellfun(@(spans) [spans{:}], allInSpan,'uni',false);
+wing_ext_frames_13frames=cellfun(@(indiv,catspan) indiv(catspan,:),ind_data,catspans,'UniformOutput',false);
+wing_ext_frames_indexed=cellfun(@(cell1,cell2) {cell1,cell2}, wing_ext_frames_13frames,num2cell(indices),'UniformOutput',false);
