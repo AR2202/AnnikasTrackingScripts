a6213bea277d41508cd872c11f9d28b69e232461
Annika Rings <annika.rings@dpag.ox.ac.uk>
2018-11-19 17:55:39 +0000

    added files for output of left-right table - deleted error handling wrapper because it was a duplicate from TheCompleteFlytrackingBundle

---
+++ run_left_right.m
@@ -0,0 +1,35 @@
+
+
+dirs = dir('*Courtship');
+
+for p = 1:numel(dirs)
+    if ~dirs(p).isdir
+        continue;
+    end
+    dirname = dirs(p).name;
+    if ismember(dirname,{'.','..'})
+        continue;
+    end
+    startdir = pwd;
+    cd(dirname);
+    
+    subdirs=dir();
+    for q = 1:numel(subdirs)
+        if ~subdirs(q).isdir
+            continue;
+        end
+        subdirname=subdirs(q).name;
+        if ismember(subdirname,{'.','..'})
+            continue;
+        end
+        disp(['Now printing left-right for:' subdirname]);
+       
+        error_handling_wrapper('left_right_table_errors.log','left_right_table',subdirname);
+       
+        cd (startdir);
+        cd(dirname);
+    end
+    cd (startdir);
+end
+
+
