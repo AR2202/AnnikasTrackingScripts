af0152b8fdb863f88280dcf669a78a4666f023bf
Annika Rings <annika.rings@dpag.ox.ac.uk>
2018-11-19 17:23:44 +0000

    added function for printing left-right assignment to a file

---
+++ left_right_table.m
@@ -0,0 +1,15 @@
+function left_right_table(inputdir)
+inputdir_JAABA=strcat(inputdir,'_JAABA');
+outputpath=fullfile(inputdir,'Results',strcat(inputdir,'left-right.csv'));
+inputpath=fullfile(inputdir,inputdir,inputdir_JAABA,'trx_id_corrected.mat');
+load(inputpath);
+disp(inputpath);
+id=arrayfun(@(i) i.id, trx);
+startpos=arrayfun(@(pos) pos.startpos, trx);
+id=transpose(id);
+startpos=transpose(startpos);
+Toutput=table(id,startpos);
+disp(Toutput);
+disp(outputpath);
+writetable(Toutput,outputpath,'WriteVariableNames',true); 
+       
