7bec9f0b330aaef680364603cdd330d8011f4eac
Annika Rings <annika.rings@dpag.ox.ac.uk>
2019-01-31 10:33:40 +0000

    deleted the reassign_identities scripts because of duplication to theCompleteFlyTrackingBundle repository

--- reassign_identities/reassign_identities.m
+++
@@ -1,7 +0,0 @@
-function reassign_identities(inputdir)
-JAABAfolder=strcat(inputdir,'_JAABA');
-JAABAfolder_path=fullfile(inputdir,JAABAfolder);
-id_path=fullfile(inputdir,'ids.mat');
-assign_chambers(inputdir);
-assign_score_identities(JAABAfolder_path,id_path);
-end
