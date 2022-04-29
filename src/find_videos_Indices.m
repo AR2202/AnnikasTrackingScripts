5cd6da7ceb89a3322559321f7fcb0fd7dea9d057
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-04-29 18:57:55 +0100

    modified find videos Indices script for new recording format - wip

--- src/find_videos_Indices.m
+++ src/find_videos_Indices.m
@@ -12,18 +12,26 @@
 %videoname,fly-id,delimitor(in file name, usually '_')
 %must be run inside the directory that contains the folders named
 %xxx_Courtship which contain the video folders
+%olddataformat: whether old recording setup was used
 
-function find_videos_Indices(genotypelist, genotype)
+function find_videos_Indices(genotypelist, genotype, olddataformat)
+switch nargin
+    case 3
+        olddataformat = olddataformat;
+        
+    otherwise
+        olddataformat = true;
+end
 outputtable = readtable(genotypelist, 'readvariablenames', false);
 disp(outputtable);
 outputvar2 = arrayfun(@(input) input, outputtable.Var2, 'UniformOutput', false);
 
 startdir = pwd;
-data = [];
-data_approach = [];
-data_CI = [];
-data_init = [];
-data_cop = [];
+data.wing = [];
+data.approach = [];
+data.CI = [];
+data.init = [];
+data.cop = [];
 dirs = dir();
 
 for p = 1:numel(dirs)
@@ -74,111 +82,125 @@ for p = 1:numel(dirs)
                         disp(r);
                         disp('Fly ID');
                         disp(newtable3.Var2(r));
-
-                        if isnumeric(outputtable2.WingIndex(outputtable2.FlyId == newtable3.Var2(r)))
-                            WingIndex = outputtable2.WingIndex(outputtable2.FlyId == newtable3.Var2(r));
+                        disp(outputtable2);
+                        datanames = {'wing','approach','CI'};
+                        if olddataformat
+                            indices_names = {'WingIndex','ApproachingIndex','CourtshipIndex'};
                         else
-                            WingIndex = str2double(outputtable2.WingIndex(outputtable2.FlyId == newtable3.Var2(r)));
+                            indices_names ={'wing', 'approaching','CI'};
                         end
-                        disp('Wing Index:');
-                        disp(WingIndex);
+                            for index_num = 1: length(indices_names)
+                                index = indices_names{index_num};
+                                dataname = datanames{index_num};
+                                
 
+                       
+                       if isnumeric(outputtable2.(index)(outputtable2.FlyId == newtable3.Var2(r)))
+                          CurrIndex = outputtable2.(index)(outputtable2.FlyId == newtable3.Var2(r));
+                       else
+                            CurrIndex = str2double(outputtable2.(index)(outputtable2.FlyId == newtable3.Var2(r)));
+                        end
+                        disp(index);
+                        disp(CurrIndex);
+                            
                         if ismember(newtable3.Var2(r), outputtable2.FlyId)
 
-                            if (size(WingIndex) > 0)
+                            if (size(CurrIndex) > 0)
 
-                                data = [data, WingIndex];
+                                data.(dataname) = [data.(dataname), CurrIndex];
                             end
-
-                        end
-                        if isnumeric(outputtable2.ApproachingIndex(outputtable2.FlyId == newtable3.Var2(r)))
-                            ApproachingIndex = outputtable2.ApproachingIndex(outputtable2.FlyId == newtable3.Var2(r));
-                        else
-                            ApproachingIndex = str2double(outputtable2.ApproachingIndex(outputtable2.FlyId == newtable3.Var2(r)));
-                        end
-                        disp('Approaching Index:');
-                        disp(ApproachingIndex);
-
-                        if ismember(newtable3.Var2(r), outputtable2.FlyId)
-
-                            if (size(ApproachingIndex) > 0)
-
-                                data_approach = [data_approach, ApproachingIndex];
-                            end
-
-                        end
-                        if isnumeric(outputtable2.CourtshipIndex(outputtable2.FlyId == newtable3.Var2(r)))
-                            CourtshipIndex = outputtable2.CourtshipIndex(outputtable2.FlyId == newtable3.Var2(r));
-                        else
-                            CourtshipIndex = str2double(outputtable2.CourtshipIndex(outputtable2.FlyId == newtable3.Var2(r)));
-                        end
-                        disp('Courtship Index:');
-                        disp(CourtshipIndex);
-
-                        if ismember(newtable3.Var2(r), outputtable2.FlyId)
-
-                            if (size(CourtshipIndex) > 0)
-
-                                data_CI = [data_CI, CourtshipIndex];
-                            end
-
-                        end
-                        if isnumeric(outputtable2.CourtshipInitiation(outputtable2.FlyId == newtable3.Var2(r)))
-                            CourtshipInit = outputtable2.CourtshipInitiation(outputtable2.FlyId == newtable3.Var2(r));
-                        else
-                            CourtshipInit = str2double(outputtable2.CourtshipInitiation(outputtable2.FlyId == newtable3.Var2(r)));
-                        end
-                        disp('Courtship Initiation:');
-                        disp(CourtshipInit);
-
-                        if ismember(newtable3.Var2(r), outputtable2.FlyId)
-
-                            if (size(CourtshipInit) > 0)
-
-
-                                init = CourtshipInit;
-                            else
-                                init = NaN;
-                            end
-                            data_init = [data_init, init];
+                             
                         end
 
-
-                        if isnumeric(outputtable2.CourtshipTermination(outputtable2.FlyId == newtable3.Var2(r)))
-                            CourtshipTermination = outputtable2.CourtshipTermination(outputtable2.FlyId == newtable3.Var2(r));
-
-                        else
-                            CourtshipTermination = str2double(outputtable2.CourtshipTermination(outputtable2.FlyId == newtable3.Var2(r)));
                         end
-                        disp('Courtship Termination:');
-                        disp(CourtshipTermination);
-                        if isnumeric(outputtable2.CourtshipDuration(outputtable2.FlyId == newtable3.Var2(r)))
-                            CourtshipDuration = outputtable2.CourtshipDuration(outputtable2.FlyId == newtable3.Var2(r));
+%                         if isnumeric(outputtable2.ApproachingIndex(outputtable2.FlyId == newtable3.Var2(r)))
+%                             ApproachingIndex = outputtable2.ApproachingIndex(outputtable2.FlyId == newtable3.Var2(r));
+%                         else
+%                             ApproachingIndex = str2double(outputtable2.ApproachingIndex(outputtable2.FlyId == newtable3.Var2(r)));
+%                         end
+%                         disp('Approaching Index:');
+%                         disp(ApproachingIndex);
+% 
+%                         if ismember(newtable3.Var2(r), outputtable2.FlyId)
+% 
+%                             if (size(ApproachingIndex) > 0)
+% 
+%                                 data_approach = [data_approach, ApproachingIndex];
+%                             end
+% 
+%                         end
+%                         if isnumeric(outputtable2.CourtshipIndex(outputtable2.FlyId == newtable3.Var2(r)))
+%                             CourtshipIndex = outputtable2.CourtshipIndex(outputtable2.FlyId == newtable3.Var2(r));
+%                         else
+%                             CourtshipIndex = str2double(outputtable2.CourtshipIndex(outputtable2.FlyId == newtable3.Var2(r)));
+%                         end
+%                         disp('Courtship Index:');
+%                         disp(CourtshipIndex);
+% 
+%                         if ismember(newtable3.Var2(r), outputtable2.FlyId)
+% 
+%                             if (size(CourtshipIndex) > 0)
+% 
+%                                 data_CI = [data_CI, CourtshipIndex];
+%                             end
+% 
+%                         end
+%                         if isnumeric(outputtable2.CourtshipInitiation(outputtable2.FlyId == newtable3.Var2(r)))
+%                             CourtshipInit = outputtable2.CourtshipInitiation(outputtable2.FlyId == newtable3.Var2(r));
+%                         else
+%                             CourtshipInit = str2double(outputtable2.CourtshipInitiation(outputtable2.FlyId == newtable3.Var2(r)));
+%                         end
+%                         disp('Courtship Initiation:');
+%                         disp(CourtshipInit);
+% 
+%                         if ismember(newtable3.Var2(r), outputtable2.FlyId)
+% 
+%                             if (size(CourtshipInit) > 0)
+% 
+% 
+%                                 init = CourtshipInit;
+%                             else
+%                                 init = NaN;
+%                             end
+%                             data_init = [data_init, init];
+%                         end
+% 
+% 
+%                         if isnumeric(outputtable2.CourtshipTermination(outputtable2.FlyId == newtable3.Var2(r)))
+%                             CourtshipTermination = outputtable2.CourtshipTermination(outputtable2.FlyId == newtable3.Var2(r));
+% 
+%                         else
+%                             CourtshipTermination = str2double(outputtable2.CourtshipTermination(outputtable2.FlyId == newtable3.Var2(r)));
+%                         end
+%                         disp('Courtship Termination:');
+%                         disp(CourtshipTermination);
+%                         if isnumeric(outputtable2.CourtshipDuration(outputtable2.FlyId == newtable3.Var2(r)))
+%                             CourtshipDuration = outputtable2.CourtshipDuration(outputtable2.FlyId == newtable3.Var2(r));
+% 
+%                         else
+%                             CourtshipDuration = str2double(outputtable2.CourtshipDuration(outputtable2.FlyId == newtable3.Var2(r)));
+%                         end
+%                         disp('Courtship Duration:');
+%                         disp(CourtshipDuration);
+% 
+% 
+%                         if ismember(newtable3.Var2(r), outputtable2.FlyId)
+% 
+%                             if (size(CourtshipTermination) > 0)
+% 
+%                                 if CourtshipTermination >= 900 && CourtshipDuration >= 600
+%                                     timeToCop = NaN;
+%                                 else
+% 
+%                                     timeToCop = CourtshipTermination;
+%                                 end
+%                             else
+%                                 timeToCop = NaN;
+%                             end
+%                             data_cop = [data_cop, timeToCop];
 
-                        else
-                            CourtshipDuration = str2double(outputtable2.CourtshipDuration(outputtable2.FlyId == newtable3.Var2(r)));
-                        end
-                        disp('Courtship Duration:');
-                        disp(CourtshipDuration);
 
-
-                        if ismember(newtable3.Var2(r), outputtable2.FlyId)
-
-                            if (size(CourtshipTermination) > 0)
-
-                                if CourtshipTermination >= 900 && CourtshipDuration >= 600
-                                    timeToCop = NaN;
-                                else
-
-                                    timeToCop = CourtshipTermination;
-                                end
-                            else
-                                timeToCop = NaN;
-                            end
-                            data_cop = [data_cop, timeToCop];
-
-
-                        end
+                        %end
                     end
                 end
             end
@@ -189,18 +211,7 @@ for p = 1:numel(dirs)
     end
     cd(startdir);
 end
-fullfigname = strcat(genotype, '_mean_WingIndex');
-datafilename = strcat(fullfigname, '.mat');
+
+datafilename = strcat(genotype, '_Indices.mat');
 save(datafilename, 'data');
-fullfigname_approach = strcat(genotype, '_mean_ApproachingIndex');
-datafilename = strcat(fullfigname_approach, '.mat');
-save(datafilename, 'data_approach');
-fullfigname_CI = strcat(genotype, '_mean_CI');
-datafilename = strcat(fullfigname_CI, '.mat');
-save(datafilename, 'data_CI');
-fullfigname_init = strcat(genotype, '_courtshipInitiation');
-datafilename = strcat(fullfigname_init, '.mat');
-save(datafilename, 'data_init');
-fullfigname_cop = strcat(genotype, '_timeToCop');
-datafilename = strcat(fullfigname_cop, '.mat');
-save(datafilename, 'data_cop');
+
