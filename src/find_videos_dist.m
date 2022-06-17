269271a8d40dd5f2cc9cba6e20704ef39a9714c2
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-06-17 13:55:53 +0100

    added support for new tracking format to find_videos_dist

--- src/find_videos_dist.m
+++ src/find_videos_dist.m
@@ -5,12 +5,23 @@
 %genotypelist
 %genotype is just the label for the outputdatafile
 %both arguments are of type string
-function find_videos_dist(genotypelist, genotype)
+%optional argument olddataformat (bool): whether old or new tracking format was
+%used; if omitted, it is assumed to be true
+function find_videos_dist(genotypelist, genotype, olddataformat)
+switch nargin
+    case 3
+        olddataformat = olddataformat;
+        
+    otherwise
+        olddataformat = true;
+end
 
 path = 'distance_travelled';
 structname = 'data';
 expname = 'dist';
-outputtable = readtable(genotypelist, 'readvariablenames', false);
+[outputtable, outputdir] = load_input_Indices(genotypelist, olddataformat);
+
+
 
 outputvar2 = arrayfun(@(input) input, outputtable.Var2, 'UniformOutput', false);
 
@@ -18,7 +29,7 @@ startdir = pwd;
 dat = [];
 dirs = dir();
 fieldname = char(strcat('F.', expname));
-
+dirnames = {};
 for p = 1:numel(dirs)
     if ~dirs(p).isdir
         continue;
@@ -27,7 +38,29 @@ for p = 1:numel(dirs)
     if ismember(dirname, {'.', '..'})
         continue;
     end
-
+if olddataformat
+      dirnames{end+1} =  dirname;
+    else
+        subdirs = dir(dirname);
+        tracked_subdirs= subdirs(~strcmp({subdirs.name}, '.')&[subdirs.isdir]==1&contains({subdirs.name},'Tracked'));
+        %convert to table for sorting
+        T = struct2table(tracked_subdirs);
+        %sort by tracking date, newest first (based on name of directory)
+        sortedT = sortrows(T, 'name', 'descend');
+        sortedSubdirs = table2struct(sortedT);
+        if numel(sortedSubdirs) >= 1
+            % this will use the newest tracking only. 
+            %If this behaviour is not intended, it needs to be changed
+            subdirname = sortedSubdirs(1).name;
+            dirname_full = fullfile(dirname,subdirname);
+            dirnames{end+1} =  dirname_full;
+        end
+        
+       
+end
+end
+for p = 1:numel(dirnames)
+    dirname = dirnames{p};
     disp(['Now looking in: ', dirname]);
     cd(dirname);
     videos = cellfun(@(list)dir(char(strcat('*', list))), outputtable.Var1, 'UniformOutput', false);
