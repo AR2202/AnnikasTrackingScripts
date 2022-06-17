0f40d33b3315f5c72a72dedf8c322396d7e702f1
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-06-17 15:45:33 +0100

    added function for finding the distance travelled for the other fly

--- src/find_videos_dist_other.m
+++ src/find_videos_dist_other.m
@@ -1,13 +1,14 @@
 %Annika Rings July 2019
-%FIND_VIDEOS_DIST(genotypelist,genotype)
+%FIND_VIDEOS_DIST_OTHER(genotypelist,genotype)
 %
-%averages the distance_travelled data for the flies specified in
-%genotypelist
+%averages the distance_travelled data for the flies paired with the flies
+%specified in genotypelist
+%i.e. the flies in the same chamber as the flies in genotypelist
 %genotype is just the label for the outputdatafile
 %both arguments are of type string
 %optional argument olddataformat (bool): whether old or new tracking format was
 %used; if omitted, it is assumed to be true
-function find_videos_dist(genotypelist, genotype, olddataformat)
+function find_videos_dist_other(genotypelist, genotype, olddataformat)
 switch nargin
     case 3
         olddataformat = olddataformat;
@@ -19,7 +20,9 @@ end
 
 expname = 'dist';
 [outputtable, outputdir] = load_input_Indices(genotypelist, olddataformat);
-
-fullfigname = strcat(genotype, '_mean_', expname);
+outputvar2 = arrayfun(@(input) input, outputtable.Var2);
+Var4 = arrayfun(@(var2)var2-(~isOdd(var2))+(isOdd(var2)), outputvar2);
+outputtable.Var2 = Var4;
+fullfigname = strcat(genotype, '_mean_', expname, '_other');
 find_dist(outputtable, outputdir, fullfigname, expname, olddataformat);
 
