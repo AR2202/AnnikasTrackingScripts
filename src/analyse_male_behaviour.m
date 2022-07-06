17502778a07a9b1062d73950313bfa9da1f5d028
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-07-06 14:45:09 +0100

    changed analyse_female_behaviour and analyse_male_behaviour to take in multiple genotypelists at a time

--- src/analyse_male_behaviour.m
+++ src/analyse_male_behaviour.m
@@ -1,5 +1,9 @@
-function analyse_male_behaviour(genotypelist, genotype, olddataformat)
+function analyse_male_behaviour(genotypelists, genotypes, olddataformat)
 run_distance_travelled(600)
-find_videos_Indices(genotypelist, genotype, olddataformat)
-find_videos_bilateral(genotypelist, genotype, olddataformat)
-find_videos_dist_other(genotypelist, genotype, olddataformat)
+for i = 1:length(genotypelists)
+    genotypelist = genotypelists{i};
+    genotype = genotypes{i};
+    find_videos_Indices(genotypelist, genotype, olddataformat)
+    find_videos_bilateral(genotypelist, genotype, olddataformat)
+    find_videos_dist_other(genotypelist, genotype, olddataformat)
+end
