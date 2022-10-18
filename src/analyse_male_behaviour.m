d22c56792f49bc7970880e9f5d4008bda5656710
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-10-18 19:29:48 +0100

    changed male behaviour

--- src/analyse_male_behaviour.m
+++ src/analyse_male_behaviour.m
@@ -1,4 +1,5 @@
 function analyse_male_behaviour(genotypelists, genotypes, olddataformat)
 run_distance_travelled(600)
 run_mean_dist_to_other()
+run_mean_velocity(900)
 find_videos_male_behaviour(genotypelists, genotypes, olddataformat)
